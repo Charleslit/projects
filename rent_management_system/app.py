05383a9a862b442c6272a38d9df2cb0333cb6635
litmus <litmuspower@gmail.com>
2023-02-09 05:21:35 +0300

    rent_management_system

---
+++ rent_management_system/app.py
@@ -0,0 +1,24 @@
+rooms = input("input the no of rooms occupied:")
+
+
+tenants = input("input the names of tenants:")
+
+
+
+rent  = input(  "enter rent payaple per room :")
+ammount = input( )
+x=rent
+y= ammount
+balance=x-y 
+print(balance*/-)
+balance = int("rent") - int("ammount")
+
+if (balance > 0):
+
+    print("you have a balance of " 
+          "kindly pay up to finish your rent ")
+
+elif (balance < 0):
+      print("you have paid extra"    " rent for this month ")
+elif (balance == 0):
+      print("ypu have cleared rent for this month  ")
