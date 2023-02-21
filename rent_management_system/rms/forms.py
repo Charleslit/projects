290a18437a4d3918fc0f607987f3080b564b1183
litmus <litmuspower@gmail.com>
2023-02-21 12:30:49 +0300

    mult

--- rent_management_system/rms/forms.py
+++ rent_management_system/rms/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length, Email ,EqualTo, ValidationError
 from rms.models import User
@@ -29,4 +30,19 @@ class LoginForm(FlaskForm):
                        validators=[DataRequired(),Email()])
       password= PasswordField('password', validators=[DataRequired()])
       remember=BooleanField('remember me')
-      submit = SubmitField('Login')
+      submit = SubmitField('Login') 
+
+class UpdateAccountForm(FlaskForm):
+   username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+   email = StringField('Email', 
+                       validators=[DataRequired(),Email()])
+   submit = SubmitField('Update')
+   def validate_username(self , username):
+      user = User.query.filter_by(username= username.data).first()
+      if user:
+         raise ValidationError('that username is taken. please choose another one. ')
+   def validate_email(self , email):
+      user = User.query.filter_by(email= email.data).first()
+      if user:
+         raise ValidationError('that email is taken. please choose another one. ')
--- rent_management_system/rms/routes.py
+++ rent_management_system/rms/routes.py
@@ -1,8 +1,8 @@
-from flask import  render_template ,flash, redirect ,url_for
+from flask import  render_template ,flash, redirect ,url_for, request
 from rms import app, db ,bcrypt
 from rms.forms import RegistrationForm,LoginForm
 from rms.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user,logout_user,login_required
 
 posts =[
       {'author':'jane doue',
@@ -33,6 +33,8 @@ def about():
 
 @app.route('/Register',methods=['GET', 'POST'])
 def register():
+      if current_user.is_authenticated:
+            return redirect(url_for('Home'))
       form = RegistrationForm()
       if form.validate_on_submit():
            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf_8')
@@ -44,15 +46,27 @@ def register():
       return render_template('register.html', title='Register', form=form )
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+      if current_user.is_authenticated:
+            return redirect(url_for('Home'))
       form = LoginForm()
       if form.validate_on_submit():
            user = User.query.filter_by(email= form.email.data).first()
            if user and bcrypt.check_password_hash(user.password, form.password.data):
                login_user(user,remember=form.remember.data)
+               next_page = request.args.get('next')
                flash('you have been logged in succcesfully', 'success')
                return redirect(url_for('Home'))
            else:
                 flash('login unsuccesfull . please check username and password', 'danger')
       return render_template('login.html', title='Login', form= form )
 
-   
+@app.route('/logout')
+def logout():
+      logout_user()
+      return redirect(url_for('Home'))
+
+@app.route('/account')
+@login_required
+def account():
+      image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+      return render_template('account.html', title='account',image_file=image_file ) 
