08368d8a2f2220f6bd8e4771e4cfe180a69feb47
litmus <litmuspower@gmail.com>
2023-02-23 08:09:00 +0300

    m

--- rent_management_system/rms/routes.py
+++ rent_management_system/rms/routes.py
@@ -69,6 +69,12 @@ def logout():
 @login_required
 def account():
       form = UpdateAccountForm()
+      if form.validate_on_submit():
+            current_user.username = form.username.data
+            current_user.email = form.email.data
+            db.session.commit()
+            flash('your account has been updated succesfully ', 'success')
+            return redirect(url_for('account'))
       image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
       return render_template('account.html', title='account',image_file=image_file ,
                                         form=form, users=users) 
