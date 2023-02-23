cb6db14d56ae55733f05bb9ff5d27e752f2642ea
litmus <litmuspower@gmail.com>
2023-02-23 09:53:27 +0300

    added update feature

--- rent_management_system/rms/forms.py
+++ rent_management_system/rms/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField , FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length, Email ,EqualTo, ValidationError
@@ -38,7 +39,7 @@ class UpdateAccountForm(FlaskForm):
    email = StringField('Email', 
                        validators=[DataRequired(),Email()])
    
-   
+   picture = FileField('update profile picture',validators=[FileAllowed(['jpg', 'png'])])
    submit = SubmitField('Update')
    
    def validate_username(self , username):
--- rent_management_system/rms/routes.py
+++ rent_management_system/rms/routes.py
@@ -1,3 +1,6 @@
+import os 
+import secrets
+from PIL import Image
 from flask import  render_template ,flash, redirect ,url_for, request
 from rms import app, db ,bcrypt
 from rms.forms import RegistrationForm,LoginForm,UpdateAccountForm
@@ -64,17 +67,36 @@ def login():
 def logout():
       logout_user()
       return redirect(url_for('Home'))
+def save_picture(form_picture):
+      random_hex = secrets.token_hex(8)
+      _, f_ext = os.path.splitext(form_picture.filename)
+      picture_fn = random_hex + f_ext
+      picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+      
+      output_size = (125, 125)
+      i = Image.open(form_picture)
+      i.thumbnail(output_size)
+      i.save(picture_path)
 
-@app.route('/account')
+      return picture_fn
+
+@app.route('/account' ,methods=['GET','POST'])
 @login_required
 def account():
       form = UpdateAccountForm()
       if form.validate_on_submit():
+            if form.picture.data:
+                  picture_file = save_picture(form.picture.data)
+                  current_user.image_file = picture_file
             current_user.username = form.username.data
             current_user.email = form.email.data
             db.session.commit()
             flash('your account has been updated succesfully ', 'success')
             return redirect(url_for('account'))
+            
+      elif request.method == 'GET':
+                  form.username.data = current_user.username
+                  form.email.data = current_user.email
       image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
       return render_template('account.html', title='account',image_file=image_file ,
                                         form=form, users=users) 
