0ba06be32bf55a0211a524025dd2c9bfd8fbbfb9
litmus <litmuspower@gmail.com>
2023-02-24 13:13:15 +0300

    c

--- rent_management_system/rms/models.py
+++ rent_management_system/rms/models.py
@@ -17,11 +17,11 @@ class User(db.Model, UserMixin):
      posts = db.relationship('Post',backref='author',lazy=True)
      
      
-    def get_reset_token(self, expires_sec=1800)
+def get_reset_token(self, expires_sec=1800):
           s = Serializer(app.config['SECRET_KEY'], expires_sec)
           return s.dumps({'user_id': self.id}).decode('utf-8')
- @staticmethod
-    def verify_reset_token(token):
+@staticmethod
+def verify_reset_token(token):
           s = Serializer(app,config['SECRET_KEY'])
           try:
                user_id = s.loads(token)['user_id']
@@ -29,7 +29,7 @@ class User(db.Model, UserMixin):
                     return None
           return User.query.get(user_id)
 
-     def __repr__(self):
+def __repr__(self):
           return f"User('{self.username}','{self.password}', '{self.image_file}')"
      
 class Post(db.Model):
